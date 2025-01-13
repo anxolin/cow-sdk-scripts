777d9db56b2f922d80d8076beae1024edae672e1
Anxo Rodriguez <anxolin@gmail.com>
2025-01-13 08:01:05 +0000

    Add limit order

---
+++ src/TradeSDK/limitSell.ts
@@ -0,0 +1,43 @@
+import { APP_CODE, COW_ADDRESS, WETH_ADDRESS } from "../const";
+import {
+  SupportedChainId,
+  OrderKind,
+  TradeParameters,
+  TradingSdk,
+  LimitTradeParameters,
+} from "@cowprotocol/cow-sdk";
+import { ethers } from "ethers";
+import { getPk } from "../common/utils";
+
+export async function run() {
+  // Set up provider and wallet
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(getPk(), provider);
+
+  // Initialize the SDK with the wallet
+  const sdk = new TradingSdk({
+    chainId: SupportedChainId.SEPOLIA,
+    signer: wallet, // Use a signer
+    appCode: APP_CODE,
+  });
+
+  // Define trade parameters
+  console.log("Limit Order: Sell 0.1 WETH for at least 1 wei of COW");
+  const parameters: LimitTradeParameters = {
+    kind: OrderKind.SELL, // Sell
+    sellAmount: ethers.utils.parseUnits("0.1", 18).toString(), // 0.1 WETH
+    buyAmount: "1", // 1 wei of COW (pure market order)
+    sellToken: WETH_ADDRESS,
+    sellTokenDecimals: 18,
+    buyToken: COW_ADDRESS, // For COW
+    buyTokenDecimals: 18,
+    slippageBps: 50,
+  };
+
+  // Post the order
+  const orderId = await sdk.postLimitOrder(parameters);
+
+  console.log(
+    `Order created, id: https://explorer.cow.fi/sepolia/orders/${orderId}?tab=overview`
+  );
+}
