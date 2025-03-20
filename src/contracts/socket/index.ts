7f46ecb90f2790895c732c2c545585ad649bca63
Sebastian T F <sebastian.tharakan97@gmail.com>
2025-03-20 16:39:59 +0530

    feat: socket cctp, across scripts

---
+++ src/contracts/socket/index.ts
@@ -0,0 +1,166 @@
+import { SupportedChainId } from '@cowprotocol/cow-sdk';
+import {
+  Contract as WeirollContract,
+  Planner as WeirollPlanner,
+} from '@weiroll/weiroll.js';
+import { ethers } from 'ethers';
+import { BaseTransaction } from '../../types';
+import { getWallet } from '../../utils';
+import { getCowShedAccount } from '../cowShed';
+import { getErc20Contract } from '../erc20';
+import { CommandFlags, getWeirollTx } from '../weiroll';
+import { bytesLibAbi, socketGatewayAbi } from './types';
+import {
+  decodeBungeeTxData,
+  getBungeeQuote,
+  getBungeeRouteTransactionData,
+} from './utils';
+
+export interface BridgeWithBungeeParams {
+  owner: string;
+  sourceChain: SupportedChainId;
+  sourceToken: string;
+  sourceTokenAmount: bigint;
+  targetToken: string;
+  targetChain: number;
+  recipient: string;
+  includeBridges: string[];
+}
+
+export async function bridgeWithBungee(
+  params: BridgeWithBungeeParams
+): Promise<BaseTransaction> {
+  const {
+    owner,
+    sourceChain,
+    sourceToken,
+    sourceTokenAmount,
+    targetChain,
+    targetToken,
+    recipient,
+    includeBridges,
+  } = params;
+
+  // Get cow-shed account
+  const cowShedAccount = getCowShedAccount(sourceChain, owner);
+
+  const planner = new WeirollPlanner();
+
+  // Get bungee quote
+  const quote = await getBungeeQuote({
+    fromChainId: sourceChain.toString(),
+    fromTokenAddress: sourceToken,
+    toChainId: targetChain.toString(),
+    toTokenAddress: targetToken,
+    fromAmount: sourceTokenAmount.toString(),
+    userAddress: cowShedAccount, // bridge input token will be in cowshed account
+    recipient: recipient,
+    sort: 'output', // optimize for output amount
+    singleTxOnly: true, // should be only single txn on src chain, no destination chain txn
+    isContractCall: true, // get quotes that are compatible with contracts
+    disableSwapping: true, // should not show routes that require swapping
+    includeBridges,
+  });
+  if (!quote) {
+    throw new Error('No quote found');
+  }
+  console.log('🔗 Socket quote:', quote);
+  // check if routes are found
+  if (!quote.result.routes.length) {
+    throw new Error('No routes found');
+  }
+  // check if only single user tx is present
+  if (quote.result.routes[0].userTxs.length > 1) {
+    throw new Error('Multiple user txs found');
+  }
+  // check if the user tx is fund-movr
+  if (quote.result.routes[0].userTxs[0].userTxType !== 'fund-movr') {
+    throw new Error('User tx is not fund-movr');
+  }
+
+  // use the first route to prepare the bridge tx
+  const route = quote.result.routes[0];
+  const txData = await getBungeeRouteTransactionData(route);
+  const { routeId, encodedFunctionData } = decodeBungeeTxData(
+    txData.result.txData
+  );
+
+  // Create bridged token contract
+  const bridgedTokenContract = WeirollContract.createContract(
+    getErc20Contract(sourceToken),
+    CommandFlags.CALL
+  );
+
+  // Get balance of CoW shed proxy
+  console.log(
+    `[socket] Get cow-shed balance for ERC20.balanceOf(${cowShedAccount}) for ${bridgedTokenContract.address}`
+  );
+
+  // Get bridged amount (balance of the intermediate token at swap time)
+  const sourceAmountIncludingSurplusBytes = planner.add(
+    bridgedTokenContract.balanceOf(cowShedAccount).rawValue()
+  );
+
+  // Check & set allowance for SocketGateway to transfer bridged tokens
+  // check if allowance is sufficient
+  const {
+    approvalData: {
+      approvalTokenAddress,
+      allowanceTarget,
+      minimumApprovalAmount,
+    },
+  } = txData.result;
+  const intermediateTokenContract = getErc20Contract(
+    approvalTokenAddress,
+    await getWallet(sourceChain)
+  );
+  const allowance = await intermediateTokenContract.allowance(
+    cowShedAccount,
+    allowanceTarget
+  );
+  console.log('current cowshed allowance', allowance);
+  if (allowance < minimumApprovalAmount) {
+    // set allowance
+    const approvalTokenContract = WeirollContract.createContract(
+      getErc20Contract(approvalTokenAddress),
+      CommandFlags.CALL
+    );
+    console.log(
+      `[socket] approvalTokenContract.approve(${allowanceTarget}, ${sourceAmountIncludingSurplusBytes}) for ${approvalTokenContract}`
+    );
+    const allowanceToSet = ethers.utils.parseUnits(
+      '1000',
+      await intermediateTokenContract.decimals()
+    );
+    planner.add(approvalTokenContract.approve(allowanceTarget, allowanceToSet));
+  }
+
+  const bytesLibContractAddress = '0x8f6BA63528De7266d8cDfDdec7ACFA8446c62aB4';
+  const bytesLibContract = WeirollContract.createContract(
+    new ethers.Contract(bytesLibContractAddress, bytesLibAbi),
+    CommandFlags.CALL
+  );
+  const encodedFunctionDataWithNewAmount = planner.add(
+    bytesLibContract.replaceBytes(
+      encodedFunctionData,
+      4, // first 4 bytes are the function selector
+      32, // first 32 bytes of the params are the amount
+      sourceAmountIncludingSurplusBytes
+    )
+  );
+
+  const socketGatewayContract = WeirollContract.createContract(
+    new ethers.Contract(txData.result.txTarget, socketGatewayAbi),
+    CommandFlags.CALL
+  );
+  // Call executeRoute on SocketGateway
+  planner.add(
+    socketGatewayContract.executeRoute(
+      routeId,
+      encodedFunctionDataWithNewAmount
+    )
+  );
+
+  // Return the transaction
+  return getWeirollTx({ planner });
+}
---
+++ src/scripts/bridging/swapAndBridgeBungeeAcrossArbitrumBase.ts
@@ -0,0 +1,207 @@
+import { APP_CODE, arbitrum, base } from '../../const';
+
+import {
+  COW_PROTOCOL_VAULT_RELAYER_ADDRESS,
+  OrderKind,
+  SupportedChainId,
+  TradeParameters,
+  TradingSdk,
+} from '@cowprotocol/cow-sdk';
+import { ethers } from 'ethers';
+
+import { MetadataApi } from '@cowprotocol/app-data';
+import { createCowShedTx } from '../../contracts/cowShed';
+import { confirm, getWallet, jsonReplacer } from '../../utils';
+
+import { getErc20Contract } from '../../contracts/erc20';
+import { bridgeWithBungee } from '../../contracts/socket';
+
+export async function run() {
+  /**
+   * Swap from USDT to USDC on Arbitrum,
+   * then bridge USDC to Base using Socket CCTP
+   */
+  const sourceChain = SupportedChainId.ARBITRUM_ONE;
+  const targetChain = SupportedChainId.BASE;
+  const sellToken = arbitrum.USDT_ADDRESS;
+  const sellTokenDecimals = 6;
+  const sellAmount = ethers.utils.parseUnits('1', sellTokenDecimals).toString();
+  const buyToken = base.USDC_ADDRESS;
+  const buyTokenDecimals = 6;
+
+  const wallet = await getWallet(sourceChain);
+  const walletAddress = await wallet.getAddress();
+  console.log('🔑 Wallet address:', walletAddress);
+
+  // Initialize the SDK with the wallet
+  const sdk = new TradingSdk({
+    chainId: sourceChain,
+    signer: wallet, // Use a signer
+    appCode: APP_CODE,
+  });
+
+  // Get the intermediary token
+  const intermediaryToken = arbitrum.USDC_ADDRESS;
+
+  // Get intermediate token decimals
+  const intermediateTokenContract = getErc20Contract(intermediaryToken, wallet);
+  const intermediateTokenDecimals = await intermediateTokenContract.decimals();
+  const intermediateTokenSymbol = await intermediateTokenContract.symbol();
+
+  // Estimate how many intermediate tokens we can bridge
+  let quote = await sdk.getQuote({
+    kind: OrderKind.SELL,
+    sellToken,
+    sellTokenDecimals,
+    buyToken: intermediaryToken,
+    buyTokenDecimals,
+    receiver: wallet.address,
+    amount: sellAmount,
+  });
+  const intermediateTokenAmount =
+    quote.quoteResults.amountsAndCosts.afterSlippage.buyAmount;
+
+  console.log('quote', JSON.stringify(quote, jsonReplacer, 2));
+
+  // Get raw transaction to bridge all available DAI from cow-shed using xDAI Bridge
+  const bridgeWithBungeeTx = await bridgeWithBungee({
+    owner: wallet.address,
+    sourceChain: sourceChain,
+    sourceToken: intermediaryToken,
+    sourceTokenAmount: intermediateTokenAmount,
+    targetChain: targetChain,
+    targetToken: buyToken,
+    recipient: wallet.address,
+    includeBridges: ['across'],
+  });
+
+  console.log(
+    '\n💰 Bridge tx:',
+    JSON.stringify(bridgeWithBungeeTx, jsonReplacer, 2)
+  );
+
+  // Sign and encode the transaction
+  const {
+    cowShedAccount,
+    preAuthenticatedTx: authenticatedBridgeTx,
+    gasLimit,
+  } = await createCowShedTx({
+    tx: bridgeWithBungeeTx,
+    chainId: sourceChain,
+    wallet,
+  });
+
+  // Define trade parameters. Sell sell token for intermediary token, to be received by cow-shed
+  const parameters: TradeParameters = {
+    kind: OrderKind.SELL, // Sell
+    amount: sellAmount,
+    sellToken,
+    sellTokenDecimals: sellTokenDecimals,
+    buyToken: intermediaryToken,
+    buyTokenDecimals: intermediateTokenDecimals,
+    partiallyFillable: false, // Fill or Kill
+    receiver: cowShedAccount,
+  };
+
+  const metadataApi = new MetadataApi();
+  const appData = await metadataApi.generateAppDataDoc({
+    appCode: APP_CODE,
+    metadata: {
+      hooks: {
+        post: [
+          {
+            callData: authenticatedBridgeTx.callData,
+            gasLimit: gasLimit.toString(),
+            target: authenticatedBridgeTx.to,
+            dappId: 'bridge-socket',
+          },
+        ],
+      },
+    },
+  });
+
+  console.log(
+    '🕣 Getting quote...',
+    JSON.stringify(parameters, jsonReplacer, 2)
+  );
+
+  quote = await sdk.getQuote(parameters, { appData });
+  const { postSwapOrderFromQuote, quoteResults } = quote;
+
+  console.log('quoteResults', {
+    amountsAndCosts: quoteResults.amountsAndCosts,
+  });
+
+  const minIntermediateTokenAmount =
+    quoteResults.amountsAndCosts.afterSlippage.buyAmount;
+  const minIntermediateTokenAmountFormatted = ethers.utils.formatUnits(
+    minIntermediateTokenAmount,
+    intermediateTokenDecimals
+  );
+  const sellAmountFormatted = ethers.utils.formatUnits(
+    sellAmount,
+    sellTokenDecimals
+  );
+
+  console.log(
+    `You will sell ${sellAmountFormatted} USDC and receive at least ${minIntermediateTokenAmountFormatted} ${intermediateTokenSymbol} (intermediate token). Then, it will be bridged to Base for USDC via CCTP via Socket.`
+  );
+
+  const confirmed = await confirm(
+    `You will bridge at least ${minIntermediateTokenAmountFormatted} ${intermediateTokenSymbol}. ok?`
+  );
+  if (!confirmed) {
+    console.log('🚫 Aborted');
+    return;
+  }
+
+  // check owner allowance to VaultRelayer
+  const vaultRelayerContract = COW_PROTOCOL_VAULT_RELAYER_ADDRESS[sourceChain];
+  const sellTokenContract = getErc20Contract(sellToken, wallet);
+  const sellTokenAllowance = await sellTokenContract.allowance(
+    walletAddress,
+    vaultRelayerContract
+  );
+  const sellTokenAllowanceFormatted = ethers.utils.formatUnits(
+    sellTokenAllowance,
+    sellTokenDecimals
+  );
+  console.log('sellTokenAllowanceFormatted', sellTokenAllowanceFormatted);
+  // If allowance is insufficient, grant allowance
+  if (sellTokenAllowanceFormatted < sellAmount) {
+    console.log('🚫 Insufficient allowance');
+    const confirmed_allowance = await confirm(
+      `Grant allowance to VaultRelayer?`
+    );
+    if (!confirmed_allowance) {
+      console.log('🚫 Aborted');
+      return;
+    }
+    const tx = await sellTokenContract.approve(
+      vaultRelayerContract,
+      // sellAmount
+      ethers.utils.parseUnits('1000', sellTokenDecimals)
+    );
+    console.log('Allowance granted tx', tx);
+    await tx.wait(2);
+  }
+
+  // Post the order
+  const orderId = await postSwapOrderFromQuote();
+
+  // Print the order creation
+  console.log(
+    `ℹ️ Order created, id: https://explorer.cow.fi/orders/${orderId}?tab=overview`
+  );
+
+  // Wait for the bridge start
+  console.log('🕣 Waiting for the bridge to start...');
+  console.log('🔗 Socket link: <URL>');
+  // TODO: Implement
+
+  // Wait for the bridging to be completed
+  console.log('🕣 Waiting for the bridging to be completed...');
+  // TODO: Implement
+
+  console.log(`🎉 The USDC is now waiting for you in Base`);
+}
